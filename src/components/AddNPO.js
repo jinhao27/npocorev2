4295f05e8daec6e84011358dfddcaa00c52966f2
Calix Huang <calix.huang1@gmail.com>
2020-05-03 14:58:22 -0700

    added bootstrap modal

--- src/components/AddNPO.js
+++ src/components/AddNPO.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class AddNPO extends React.Component {
-  render() {
-    return (
-      <button>Add NPO</button>
-    )
-  }
+// COMPONENTS
+import Modal from "react-bootstrap/Modal";
+
+function AddNPO() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const showModal = () => {
+    setIsOpen(true);
+  };
+
+  const hideModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      <button onClick={showModal}>Add NPO</button>
+
+      <Modal show={isOpen} onHide={hideModal}>
+        <Modal.Header>Hi</Modal.Header>
+        <Modal.Body>asdfasdf</Modal.Body>
+        <Modal.Footer>This is the footer</Modal.Footer>
+      </Modal>
+    </div>
+  )
 }
 
 export default AddNPO;
