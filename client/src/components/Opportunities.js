1d70edf6ba3c9a2a781046543e55f33129a10d39
Calix Huang <calix.huang1@gmail.com>
2020-06-10 11:39:41 -0700

    got basic post display running

--- client/src/components/Opportunities.js
+++ client/src/components/Opportunities.js
@@ -26,23 +26,10 @@ function Organizations() {
         // return response.json();
         return [
 {
-"causes": [
-"Advocacy and Human Rights",
-"Animal Welfare"
-],
-"targetAudiences": [
-"Teens"
-],
-"interests": [
-"Partnerships",
-"Sponsors",
-"Clients"
-],
-"posts": [
-{
 "_id": "5ee085d81221757a525dc3fb",
 "title": "test",
 "content": "test",
+"featured": true,
 "datetimePosted": "2020-06-10T07:03:52.988Z",
 "creator": {
 "causes": [
@@ -76,43 +63,7 @@ function Organizations() {
 "link": "https://www.launchtechllc.com/",
 "color": "#f28c41"
 },
-"type": "Event"
-}
-],
-"subscriptions": [],
-"_id": "5edfcc68bacc39467d54ebc2",
-"name": "test2",
-"email": "test2@mail.com",
-"description": "test2",
-"password": "sha1$56554a04$1$34756d649addc4c6ca40983fabd3c810f72c8b21",
-"location": {
-"name": ""
-},
-"idName": "test2",
-"npoScore": 57.963703715,
-"bumpedInLastHour": false,
-"__v": 0
-},
-{
-"causes": [],
-"targetAudiences": [],
-"interests": [
-"Sponsors",
-"Opportunities"
-],
-"posts": [],
-"subscriptions": [],
-"_id": "5edfcc4fbacc39467d54ebc1",
-"name": "test",
-"email": "test@mail.com",
-"description": "test",
-"password": "sha1$82184bf9$1$e7427694bbb0456add075cf1854d944d61c61b45",
-"location": {
-"name": ""
-},
-"idName": "test",
-"npoScore": 50,
-"bumpedInLastHour": false,
+"type": "Event",
 "__v": 0
 }
 ];
@@ -126,7 +77,7 @@ function Organizations() {
         setFeaturedPosts(featuredPostsArray);
       })
       .catch((err) => {
-          console.log("Exception:", err);
+        console.log("Exception:", err);
       });
   }
 
@@ -206,53 +157,31 @@ function Organizations() {
           <span></span>
         }
         <div className="organizations mt-5">
-          {featuredPosts ? featuredPosts.map((organization, key) =>
-            <div className="organization" key={key}>
-              <div className="image-cropper-container">
-                <div style={{ height: "50px" }} className="image-cropper">
-                  {organization.logo ?
-                    <img src={"https://npocore.s3-us-west-2.amazonaws.com/" + organization.logo} alt="" />
-                    :
-                    <img src="/static/img/no-logo.png" alt="" />
-                  }
-                </div>
-                {organization.verifiedNonprofit ?
-                  <img className="nonprofit-badge" src="/static/img/icons/501c3.svg" alt=""/>
-                  :
-                  <span></span>
-                }
-              </div>
-              <div>
-                <div className="organization-header">
-                  <h4><a href={"/@" + organization.idName}>{organization.name}</a></h4>
-                  <div className="organization-resources">
-                    <a href="mailto:{organization.email}" target="_">
-                      {organization.email ? <img src="/img/email.svg" alt="{organization.name} Email Address" /> : null}
-                    </a>
-                    <a href={"/@" + organization.idName}>
-                      <img style={{transform: "translateY(-2px)"}} src="/img/link.svg" alt="{organization.name}" />
-                    </a>
-                  </div>
-                </div>
-                {organization.location.name ? <div><strong>Location:</strong> {organization.location.name}</div> : <span></span>}
-                <div><strong>Target Audience:</strong> {organization.targetAudience}</div>
-                <div><strong>Cause:</strong> {organization.cause}</div>
-                <p className="organization-description"><strong>Description:</strong> {organization.description}</p>
-                <div className="text-right mb-2">
-                  <button className="btn btn-link p-0" onClick={readMore}>
+          {featuredPosts ? featuredPosts.map((post, key) =>
+            <div class="post" key={key}>
+              {post.image ?
+                <img class="post-image" src={"https://npocore.s3-us-west-2.amazonaws.com/" + post.image} alt=""/>
+                :
+                <span></span>
+              }
+              <div class="p-3">
+                <h3>{post.title}</h3>
+                <strong>By: <a href={"/@" + post.creator.idName}>{post.creator.name}</a></strong>
+                <br/>
+                <small>Posted: {post.datetimePosted}</small>
+                <p class="post-content mt-3">{post.content}</p>
+                <div class="text-right">
+                  <button id={post._id} class="btn btn-link p-0" onClick="readMore(this.id)">
                     <small>Read More</small>
                   </button>
                 </div>
-                <div className="organization-interests">
-                  {(organization.interests || []).map((interest, key) =>
-                    <small>{interest}</small>
-                  )}
-                </div>
+                <p><small class="type">{post.type}</small></p>
+                <a style={{ "background-color": post.button.color, "border-color": post.button.color }} class="solid-cta-button" href={post.button.link} target="_blank">{post.button.text}</a>
               </div>
             </div>
           ) :
           <div className="text-center">
-            <small>No featured organizations yet!</small>
+            <small>No featured posts yet!</small>
           </div>
         }
         </div>
@@ -283,11 +212,11 @@ function Organizations() {
           <span></span>
         }
 
-        <div className="organizations mt-5">
+        <div className="posts mt-5">
           {posts ? posts.map((post, key) =>
             <div class="post" key={key}>
               {post.image ?
-                <img class="post-image" src={s3Link + post.image} alt=""/>
+                <img class="post-image" src={"https://npocore.s3-us-west-2.amazonaws.com/" + post.image} alt=""/>
                 :
                 <span></span>
               }
@@ -295,7 +224,7 @@ function Organizations() {
                 <h3>{post.title}</h3>
                 <strong>By: <a href={"/@" + post.creator.idName}>{post.creator.name}</a></strong>
                 <br/>
-                <small>Posted: {post.datetimePosted.toLocaleDateString("en-US")}</small>
+                <small>Posted: {post.datetimePosted}</small>
                 <p class="post-content mt-3">{post.content}</p>
                 <div class="text-right">
                   <button id={post._id} class="btn btn-link p-0" onClick="readMore(this.id)">
